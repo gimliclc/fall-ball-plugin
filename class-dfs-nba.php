d3c85ee859d3fc2c28831be78bf5ffd90fc815d0
KJ Johnson <kj@fantasyteamadvice.com>
2017-10-04 16:30:04 -0600

    Create init function

--- class-dfs-nba.php
+++ class-dfs-nba.php
@@ -1,3 +1,17 @@
 <?php
 
 defined( 'ABSPATH' ) or die( 'Error: Class-DFS-NBA cannot be called directly!' );
+
+class DFS_NBA {
+	private static $initiated = false;
+
+  public static function init() {
+		if(! self::$initiated) {
+			self::init_hooks();
+			self::start_cron();
+			self::init_ajax();
+			self::init_js();
+			self::init_shortcodes();
+		}
+	}
+}
