800c2505628b4a58a02fb5308d0ac5e2e3e1d666
KJ Johnson <kj@fantasyteamadvice.com>
2017-10-03 22:08:12 -0600

    Add initial php code and license info

---
+++ dfs-nba.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: FTA+ DFS NBA
+ * Plugin URI: http://fantasyteamadvice.com/
+ * Description: Provides real time DFS NBA data to FTA+ users
+ * Version: 1.0.0
+ * Author: Kyle Johnson
+ * Author URI: http://fantasyteamadvice.com/
+ * License: Informal license
+ */
+
+defined( 'ABSPATH' ) or die( 'Error: Plugin cannot be called directly!' );
